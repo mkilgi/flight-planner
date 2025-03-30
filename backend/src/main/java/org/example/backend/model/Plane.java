c6c8929869b620ffb8451435d7328e2171707ff3
mkilgi <mrt.kilgi@gmail.com>
2025-03-30 18:20:13 +0300

    backend: added plane and seat entities

---
+++ backend/src/main/java/org/example/backend/model/Plane.java
@@ -0,0 +1,38 @@
+package org.example.backend.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.UUID;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+/**
+ * This Plane class is for saving different plane seating plans.
+ * But this will not be used to manage planes for flights, I will just use this
+ * for reusing the same seating plan for all flights.
+ */
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Plane {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+  @Column(nullable = false, unique = true)
+  private String model;
+
+  @OneToMany(mappedBy = "plane", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+  private List<Seat> seats;
+}
