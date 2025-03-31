14818959ac45f43137f9dcf069015b9fc7ff8571
mkilgi <mrt.kilgi@gmail.com>
2025-03-31 11:56:59 +0300

    frontend: route handler for flights endpoint

---
+++ frontend/app/api/flights/route.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+	const { searchParams } = new URL(request.url);
+	const API_URL = process.env.API_URL;
+	const backendUrl = new URL(`${API_URL}/flights`);
+
+	searchParams.forEach((value, key) => {
+		backendUrl.searchParams.append(key, value);
+	});
+
+	try {
+		const response = await fetch(backendUrl.toString());
+		const data = await response.json();
+
+		// console.log(data);
+		return NextResponse.json(data);
+	} catch (error) {
+		return NextResponse.json({ error: error }, { status: 500 });
+	}
+}
